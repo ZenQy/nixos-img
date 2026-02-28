aaa3e9a890c7e9dc8f042d43fec66f7c420a6306
ZenQy <zenqy.qin@gmail.com>
2026-02-28 18:51:15 +0800

    优化flake.nix

--- flake.nix
+++ flake.nix
@@ -15,18 +15,28 @@
       nixpkgs,
       disko,
     }:
-    with builtins;
+
     let
+      inherit (builtins)
+        filter
+        attrNames
+        readDir
+        concatMap
+        listToAttrs
+        substring
+        ;
       floder =
         dir:
-        filter (v: v != null) (
-          attrValues (mapAttrs (k: v: if v == "directory" then k else null) (readDir dir))
-        );
+        let
+          files = readDir dir;
+        in
+        filter (name: files.${name} == "directory") (attrNames files);
+
       hosts = concatMap (
         dir:
         map (subdir: {
-          system = if dir == "x86" || dir == "x86-uefi" then "x86_64-linux" else "aarch64-linux";
-          dir = dir;
+          system = if (substring 0 3 dir) == "x86" then "x86_64-linux" else "aarch64-linux";
+          inherit dir;
           name = subdir;
         }) (floder ./hosts/${dir})
       ) (floder ./hosts);
@@ -51,25 +61,21 @@
       );
 
       image =
-        hosts:
+        system:
         listToAttrs (
           map (host: {
             name = host.name;
-            value = self.nixosConfigurations.${host.name}.config.system.build.diskoImages;
-          }) hosts
+            value = nixos.${host.name}.config.system.build.diskoImages;
+          }) (filter (host: host.system == system) hosts)
         );
 
     in
 
     {
-
       nixosConfigurations = nixos;
       packages = {
-        x86_64-linux = image (filter (host: host.system == "x86_64-linux") hosts);
-
-        aarch64-linux = image (filter (host: host.system == "aarch64-linux") hosts);
-
+        x86_64-linux = image "x86_64-linux";
+        aarch64-linux = image "aarch64-linux";
       };
-
     };
 }
